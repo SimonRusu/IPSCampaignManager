18c86c18b2d2a11fc427ba9e855714724b93d30b
Simon Rusu <simonrust97@gmail.com>
2023-06-11 22:01:50 +0100

    feat: :sparkles: added dynamic graphics filters

--- IPSCM.WebClient/src/app/graphics/graphics.component.ts
+++ IPSCM.WebClient/src/app/graphics/graphics.component.ts
@@ -13,15 +13,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class GraphicsComponent {
 
   @ViewChild('chartCanvas', { static: true }) chartCanvas!: ElementRef<HTMLCanvasElement>;
-  campaignMethodPrediction: any = [];
   campaignId: any;
+  campaignMethodPrediction: any = [];
+  filteredData: any = [];
+  campaignFilteredPoints: any = [];
+  campaoignFilteredErrors: any = [];
   methods: string[] = ["WKNN", "SVR", "NuSVR", "LinearSVR"];
+  protocols: string[] = ["Eddystone", "iBeacon"];
+  channels: string[] = ["37", "38", "39"];
   selectAllMethodsCheck: boolean = false;
+  selectAllProtocolsCheck: boolean = false;
+  selectAllChannelsCheck: boolean = false;
   form!: FormGroup;
 
   ngOnInit(){
     this.form = this.formBuilder.group({
-      methods: ['', Validators.required]
+      methods: ['', Validators.required],
+      protocols: ['', Validators.required],
+      channels: ['', Validators.required]
     });
   }
 
@@ -30,15 +39,14 @@ export class GraphicsComponent {
     this.route.queryParams.subscribe(params => {
       if(params['data']){
         this.campaignId = params['data'];
-        console.log(this.campaignId)
       }
     })
 
     this.apiService.getMethodPredictionsById(this.campaignId).subscribe(res=> {
       
       if(res != 0){
-
         this.campaignMethodPrediction = res
+        this.filteredData = res
       }
     })
   }
@@ -46,10 +54,7 @@ export class GraphicsComponent {
   selectAllMethods(){
     const methodsControl = this.form.get('methods');
     const methodsToggle = this.form.get('methods')?.value;
-
-
     const allMethods = this.methods.slice();
-    console.log(allMethods)
     
     if(methodsControl){
       if (methodsToggle.length == 1) {
@@ -59,75 +64,161 @@ export class GraphicsComponent {
         methodsControl.setValue([]);
         this.selectAllMethodsCheck = false;
       }
+      this.onFilterSelected();
     }
   }
 
-  onMethodSelected(method: string){
-    console.log(method)
+  selectAllProtocols(){
+    const protocolsControl = this.form.get('protocols');
+    const protocolsToggle = this.form.get('protocols')?.value;
+
+    const allProtocols = this.protocols.slice();
+    
+    if(protocolsControl){
+      if (protocolsToggle.length == 1) {
+        protocolsControl.setValue(allProtocols);
+        this.selectAllProtocolsCheck = true;
+      } else {
+        protocolsControl.setValue([]);
+        this.selectAllProtocolsCheck = false;
+      }
+      this.onFilterSelected();
+    }
+  }
+
+  selectAllChannels(){
+    const channelsControl = this.form.get('channels');
+    const channelsToggle = this.form.get('channels')?.value;
+
+    const allChannels = this.channels.slice();
+    
+    if(channelsControl){
+      if (channelsToggle.length == 1) {
+        channelsControl.setValue(allChannels);
+        this.selectAllChannelsCheck = true;
+      } else {
+        channelsControl.setValue([]);
+        this.selectAllChannelsCheck = false;
+      }
+      this.onFilterSelected();
+    }
+  }
+
+  onFilterSelected(){
+    this.filteredData = [...this.campaignMethodPrediction];
+
+    this.filteredData = this.filterData(this.form.get('methods')?.value, "method", this.filteredData);
+    this.filteredData = this.filterData(this.form.get('protocols')?.value, "protocol", this.filteredData);
+    this.filteredData = this.filterData(this.form.get('channels')?.value, "channel", this.filteredData);
+
+    let sortedData = this.sortData()
+    this.fillPrecisionData(sortedData)
+  }
+
+  filterData(filters: any, property: string, data: any[]): any[]{
+    return data.filter((object:any) =>{
+      const methodDescription = JSON.parse(object.Method_description);
+      return filters.some((filter: string) => filter === methodDescription[property].toString());
+    })
+  }
+  
+  sortData() {
+    let parsedData = this.filteredData.map((item:any) => {
+        let predictedPoints = JSON.parse(item.Predicted_points);
+        let meanErrors = JSON.parse(item.Mean_error);
+      
+        let sortedData = Object.keys(predictedPoints).map(key => ({
+            key: key,
+            predictedPoint: predictedPoints[key],
+            meanError: meanErrors[key]
+        }));
+      
+        sortedData.sort((a, b) => a.meanError - b.meanError);
+        let cdfValues = sortedData.map((value, i) => ((i + 1) / sortedData.length) * 100);
+      
+        return {
+            ...item,
+            SortedData: sortedData,
+            CdfValues: cdfValues
+        };
+    });
+
+    return parsedData;
+  }
+
+  fillPrecisionData(filteredData:any){
+    let labels = [];
+    let datasets = [];
+
+    for(let i = 0; i < filteredData.length; i++){
+      let item = filteredData[i];
+      let sortedData = item.SortedData;
+      let meanErrors = [];
+      let cdfValues = item.CdfValues;
+
+      for(let data of sortedData){
+        meanErrors.push(data.meanError);
+      }
+
+      let hue = Math.floor((i * 360) / filteredData.length);
+      let color = `hsl(${hue}, 100%, 50%)`;
+      let backgroundColor = `hsla(${hue}, 100%, 50%, 0.2)`;
+
+
+      let dataset = {
+        data: cdfValues,
+        label: `Series ${item.Id}`,
+        backgroundColor: backgroundColor,
+        borderColor: color,
+        pointBackgroundColor: color,
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: color,
+        fill: 'origin',
+      }
+
+      datasets.push(dataset);
+      labels = meanErrors;
+    }
+
+    this.lineChartData = {
+      datasets: datasets,
+      labels: labels
+    };
+
   }
   
   public lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [ 65, 59, 80, 81, 56, 55, 40 ],
-        label: 'Series A',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-      {
-        data: [ 28, 48, 40, 19, 86, 27, 90 ],
-        label: 'Series B',
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)',
-        fill: 'origin',
-      },
-      {
-        data: [ 180, 480, 770, 90, 1000, 270, 400 ],
-        label: 'Series C',
-        yAxisID: 'y1',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        borderColor: 'red',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      }
-    ],
-    labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July' ]
+    datasets: [],
+    labels: []
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
-    elements: {
-      line: {
-        tension: 0.5
-      }
-    },
     scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
-      y:
-        {
-          position: 'left',
-        },
-      y1: {
-        position: 'right',
-        grid: {
-          color: 'rgba(255,0,0,0.3)',
-        },
-        ticks: {
-          color: 'red'
-        }
+      xAxis: {
+          title: {
+              display: true,
+              text: 'Error (m)'
+          },
+          beginAtZero: true,
+          type:"linear",
+          min: 0,
+          max: 10,
+          ticks: {
+            stepSize: 2,
+            
+          }
+      },
+      y: {
+          title: {
+              display: true,
+              text: 'Distribución acumulada (CDF)'
+          },
+          type:"linear",
+          min: 0,
+          max: 100
       }
-    },
+    }
   };
 
   public lineChartType: ChartType = 'line';
